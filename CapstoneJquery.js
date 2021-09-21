54e4223d158e1bd18ea2e77ab079dd77a6bb0208
Bailey Knezevich <69914817+baileyknez@users.noreply.github.com>
2021-09-21 18:13:21 -0400

    Update CapstoneJquery.js
    This is just the code used to get the map. I have yet to connect this JavaScript file to the HTML. This is also where we will have to deal with the school grades and add functionality to our website.

--- CapstoneJquery.js
+++ CapstoneJquery.js
@@ -1,1 +1,10 @@
-hello
+// Google Maps API key AIzaSyD__zNryK3aQH46g_4ArIrk4zYdhHr7pAo 
+
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
